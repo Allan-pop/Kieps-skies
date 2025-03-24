6c2913427dcc6679b4bc192417e4e2135071f4d3
Allan-pop <allankybz4@gmail.com.com>
2025-03-24 15:31:12 +0300

    completed

---
+++ module-1/notes/drumkit/index.js
@@ -0,0 +1,56 @@
+// Function to play sound
+function playSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            break;
+        default:
+            return;
+    }
+    audio.play();
+}
+
+// Detecting button click
+var buttons = document.querySelectorAll(".drum");
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+});
+
+// Detecting keypress
+document.addEventListener("keydown", function (event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+});
+
+// Function to add animation
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+        setTimeout(() => {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
